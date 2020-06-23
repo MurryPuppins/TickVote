8a2b622652f67d7163a71d7eb31b3c3e382436e0
Cameron Carey <cc6395@rit.edu>
2020-06-22 20:29:25 -0400

    Pre-release commit | 1 TODO

--- src/us/murrypuppins/tickvote/commands/TickVote.java
+++ src/us/murrypuppins/tickvote/commands/TickVote.java
@@ -18,7 +18,7 @@ public class TickVote implements CommandExecutor {
     private Main plugin = Main.getInstance();
     private boolean voteStatus = false;
     private long start;
-    private long end = plugin.getConfig().getLong("timer") * 1200;
+    private long end = plugin.getConfig().getInt("timer");
     private int voteCount;
     private int tickVote;
     private ArrayList<Player> pList = new ArrayList<Player>();
@@ -31,106 +31,104 @@ public class TickVote implements CommandExecutor {
 
         if(!(sender instanceof Player) && args[0].equalsIgnoreCase("reload")) {
             reloadConfig();
+            Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', prefix + "Reload complete!"));
+            return true;
         }
 
         if(!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.AQUA + prefix + "Must be a player to execute!");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Must be a player to execute!"));
             return true;
         }
         Player player = (Player) sender;
 
         if(!player.hasPermission("tv.vote")) {
-            player.sendMessage(ChatColor.AQUA + prefix + "Invalid permissions!");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Invalid permissions!"));
             return true;
         }
         if(args.length == 0) {
             if(!voteStatus) {
-                player.sendMessage(ChatColor.AQUA + prefix + "No current votes found! Start one with /tv <tick>!");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "No current votes found! Start one with /tv <tick>!"));
+                return true;
             }
             else {
-                if(System.currentTimeMillis() < start+end) {
+                if(System.currentTimeMillis() < (start+(end*60000))) {
                     if(!pList.contains(player)) {
                         voteCount++;
                         pList.add(player);
-                        player.sendMessage(ChatColor.AQUA + prefix + "Vote casted!");
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Vote casted!"));
+                        //TODO
+                        // Add config variable for chance at when vote is executed
                         if(voteCount >= (getpOnline()/2)) {
                             double cost = plugin.getConfig().getDouble("cost");
                             for (Player value : pList) {
                                 economy.withdrawPlayer(value, cost);
                             }
                             world.setTime(tickVote);
-                            Bukkit.broadcastMessage(ChatColor.AQUA + prefix + "Vote has passed, the time has been adjusted!");
+                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Vote has passed, the time has been adjusted!"));
+                            pList.clear();
+                            setVoteStatus();
                         }
                         return true;
                     }
                     else {
-                        player.sendMessage(ChatColor.AQUA + prefix + "You have already casted your vote!");
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "You have already casted your vote!"));
+                        return true;
                     }
                 }
                 else {
                     setVoteStatus();
                     voteCount = 0;
                     pList.clear();
-                    player.sendMessage(ChatColor.AQUA + prefix + "The vote has expired! Please re-issue the command to start a new vote!");
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "The vote has expired! Please re-issue the command to start a new vote!"));
                 }
             }
         }
         if(args.length == 1) {
             if(args[0].equalsIgnoreCase("reload")) {
                 if(!player.hasPermission("tv.reload")) {
-                    player.sendMessage(ChatColor.AQUA + prefix + "You do not have the reload permissions!");
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "You do not have the reload permissions!"));
                     return true;
                 }
                 reloadConfig();
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Reload complete!"));
                 return true;
             }
             if(!voteStatus && (getpOnline() > 1)) {
-                if(args[0].equalsIgnoreCase("day")) {
-                    tickVote = 1000;
+                if(!isInt(args[0])) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Tick input is unknown, try day, night or a #!"));
+                    return true;
                 }
-                if(args[0].equalsIgnoreCase("night")) {
-                    tickVote = 13000;
-                }
-                else {
-                    if(!isInt(args[0])) {
-                        player.sendMessage(ChatColor.AQUA + prefix + "Tick input is unknown, try day, night or a #!");
-                        return true;
-                    }
-                    tickVote = Integer.parseInt(args[0]);
-                }
+                setVoteStatus();
+                tickVote = Integer.parseInt(args[0]);
                 start = System.currentTimeMillis();
                 world = player.getWorld();
-                Bukkit.broadcastMessage(ChatColor.AQUA + prefix + "A day vote has been started! /tv day to vote!");
-                Bukkit.broadcastMessage(ChatColor.AQUA + prefix + "The current vote is for the tick time of " + args[0]);
-                setVoteStatus();
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + "A tick vote for the tick time of " + args[0]) + " has been started! Vote with '/tv'");
                 voteCount = 1;
                 pList.add(player);
+                return true;
             }
             if(!voteStatus && (getpOnline() == 1)) {
-                if(args[0].equalsIgnoreCase("day")) {
-                    tickVote = 1000;
+                if(!isInt(args[0])) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Tick input is unknown, try entering a number between 0 and 24000!"));
+                    return true;
                 }
-                if(args[0].equalsIgnoreCase("night")) {
-                    tickVote = 13000;
-                }
-                else {
-                    tickVote = Integer.parseInt(args[0]);
-                }
+                tickVote = Integer.parseInt(args[0]);
                 start = System.currentTimeMillis();
                 world = player.getWorld();
                 if(player.isOnline()) {
                     economy.withdrawPlayer(player, plugin.getConfig().getDouble("cost"));
                 }
                 world.setTime(tickVote);
-                Bukkit.broadcastMessage(ChatColor.AQUA + prefix + "The time has been modified!");
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + "The time has been modified!"));
                 return true;
             }
             if(voteStatus) {
-                player.sendMessage(ChatColor.AQUA + prefix + "There is an ongoing vote already, please wait until the vote is over!");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "There is an ongoing vote already, please wait until the vote is over!"));
+                return true;
             }
         }
         else {
-            player.sendMessage(ChatColor.AQUA + prefix + "Invalid input!");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Invalid input!"));
             return true;
         }
         return true;
@@ -140,6 +138,7 @@ public class TickVote implements CommandExecutor {
         plugin.saveDefaultConfig();
         plugin.reloadConfig();
         prefix = plugin.getConfig().getString("prefix");
+        end = plugin.getConfig().getInt("timer");
     }
 
     private void setVoteStatus() {
